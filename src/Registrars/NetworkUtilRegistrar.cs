8f0be0ce002a66f443d9504e1da9784ba9fd0c9d
Jake Soenneker <jake@soenneker.com>
2025-02-07 16:14:11 -0600

    registrar

--- src/Registrars/NetworkUtilRegistrar.cs
+++ src/Registrars/NetworkUtilRegistrar.cs
@@ -9,17 +9,21 @@ public static class NetworkUtilRegistrar
     /// <summary>
     /// Adds <see cref="INetworkUtil"/> as a scoped service. (Recommended) <para/>
     /// </summary>
-    public static void AddNetworkUtilAsScoped(this IServiceCollection services)
+    public static IServiceCollection AddNetworkUtilAsScoped(this IServiceCollection services)
     {
         services.TryAddScoped<INetworkUtil, NetworkUtil>();
+
+        return services;
     }
 
     /// <summary>
     /// Adds <see cref="INetworkUtil"/> as a singleton service. <para/>
     /// (Use <see cref="AddNetworkUtilAsScoped"/> unless this is being consumed by a Singleton)
     /// </summary>
-    public static void AddNetworkUtilAsSingleton(this IServiceCollection services)
+    public static IServiceCollection AddNetworkUtilAsSingleton(this IServiceCollection services)
     {
         services.TryAddSingleton<INetworkUtil, NetworkUtil>();
+
+        return services;
     }
 }
